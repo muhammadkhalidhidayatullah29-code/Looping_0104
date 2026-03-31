06646645c9470497d7a1292483545a76c55fbd83
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-03-31 21:30:54 +0700

    menambahkan if else

--- looping.cpp
+++ looping.cpp
@@ -50,4 +50,10 @@ int main (){
 
     rerata = (nilM + nilB) / 2;
 
+    if (!(rerata >= 60) ) 
+        status = "Lulus";
+     else 
+        status = "Tidak Lulus";
+    
+    cout << "Status Kelulusan = " << status << " , dengan nilai rata-rata = " << rerata << endl; 
 }
