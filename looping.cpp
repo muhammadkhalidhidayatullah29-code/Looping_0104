bf65a8cdb330e50227b2c0256bc6175485a24482
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-03-31 21:11:39 +0700

    untuk menambahkan urutan angka acak yang berbeda

--- looping.cpp
+++ looping.cpp
@@ -5,7 +5,10 @@ using namespace std;
 int main() {
     int x;
 
-    cout << "PERULANGAN FOR" << endl
+    cout << "PERULANGAN FOR" << endl;
     for(int i=0; i<=5; i++){
         cout << "Teknologi Informasi UMY"<< endl; 
     }
+    cout << endl;
+
+    srand(time(0));
