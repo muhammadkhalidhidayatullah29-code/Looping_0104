2d25283059631780b288c3d7a0d65e202fe884b4
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-03-31 21:27:33 +0700

    menambahkan intejer dan cout & cin

--- looping.cpp
+++ looping.cpp
@@ -39,3 +39,15 @@ Logika not
 #include <iostream>
 using namespace std;
 
+int main (){
+    float nilB,nilM,rerata;
+    string status;
+
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilM;
+    cout << "Masukkan nilai Bahasa Indonesia = ";
+    cin >> nilB;
+
+    rerata = (nilM + nilB) / 2;
+
+}
