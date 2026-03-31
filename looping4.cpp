7727771efdf938f5f97c7b18723327a66997cda4
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-03-31 22:19:53 +0700

    Revisi Dari file looping.cpp berubah jadi 4 file dan sudah selesai

---
+++ looping4.cpp
@@ -0,0 +1,22 @@
+//Logika AND
+#include <iostream>
+using namespace std;
+
+int main (){
+    float nilB,nilM,rerata;
+    string status;
+
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilM;
+    cout << "Masukkan nilai Bahasa Indonesia = ";
+    cin >> nilB;
+
+    rerata = (nilM + nilB) / 2;
+
+    if ((rerata >= 60 && nilM >= 70) ) 
+        status = "Lulus";
+     else 
+        status = "Tidak Lulus";
+
+    cout << "Status Kelulusan = " << status << " , dengan nilai rata-rata = " << rerata << endl;
+}
