79dfae88bcdddfc3e4339f19414664856fafe1ab
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-03-31 21:09:28 +0700

    perulangan dengan FOR, mencetak tulisan sebannyak 5 kali

--- looping.cpp
+++ looping.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
+
+int main() {
+    int x;
+
+    cout << "PERULANGAN FOR" << endl
+    for(int i=0; i<=5; i++){
+        cout << "Teknologi Informasi UMY"<< endl; 
+    }
