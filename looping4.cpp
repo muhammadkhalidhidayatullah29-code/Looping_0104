544f8701b00b689fd8f931b2e8fde7d51fb57474
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-03-31 23:53:39 +0700

    revisi ulang

--- looping4.cpp
+++ looping4.cpp
@@ -13,7 +13,7 @@ int main (){
 
     rerata = (nilM + nilB) / 2;
 
-    if ((rerata <= 60 && nilM >= 70) ) 
+    if ((rerata >= 60 && nilM >= 70) ) 
         status = "Lulus";
      else 
         status = "Tidak Lulus";
